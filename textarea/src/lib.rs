0b1b533b784d51331db6e126c0bdb8b28ee33418
Nixon Enraght-Moony <nixon.emoony@gmail.com>
2020-07-27 01:28:51 +0100

    Cursur: don't use system line numbering

--- textarea/src/lib.rs
+++ textarea/src/lib.rs
@@ -29,7 +29,7 @@ impl TextArea {
         // TODO: caching of both the format and the layout
         let font = piet_text
             // While druid uses the "toy" api, this is fine
-            .new_font_by_name("monospace", font_size)
+            .new_font_by_name("JetBrains Mono", font_size)
             .build()
             .unwrap();
 
@@ -143,20 +143,23 @@ impl Widget<EditableText> for TextArea {
 
         // Paint the background
         let clip_rect = ctx.size().to_rect();
-
         ctx.fill(clip_rect, &background_color);
 
         // Core text happens in the lambda, outside is for aux.
         ctx.with_save(|rc| {
+            // For padding stuff, I think
             rc.clip(clip_rect);
 
-            //TODO: Figure out where this 0.8 comes from
+            // TODO: Figure out where this 0.8 comes from
+            // font_size is the number of "pixels" from top to bottom,
+            // so that makes sense.
             let text_height = font_size * 0.8;
 
-            //TODO: scrolling
+            // TODO: scrolling
+            // TODO: what is this
             let text_pos = Point::new(0.0, text_height);
 
-            //TODO: Only pull the bit of the rope that's on screen.
+            // TODO: Only pull the bit of the rope that's on screen.
             // This is super wastefull, as we do a full copy out of the rope to render
             // but we need to convert to string before we sent it into druid
             // so what we should do it see what needs to be painted (ie is onscrean)
@@ -168,6 +171,10 @@ impl Widget<EditableText> for TextArea {
             // so when we implent partial views, this will need to change
             let text_byte_idx = data.rope().char_to_byte(data.curser());
 
+            // 0 indexed from top line number. We can probably cache this, but this should
+            // be quite fast (O(log n)).
+            let lineno: f64 = data.rope().char_to_line(data.curser()) as f64;
+
             // This gives us the x, but for y, it's not platform independent,
             // linux gives from the top, but macOS is on the bottom.
             // Also we need to do edge cases around the \n that this get's wrong
@@ -175,21 +182,41 @@ impl Widget<EditableText> for TextArea {
             let pos = text_layout.hit_test_text_position(text_byte_idx);
 
             // Curser rendering
-            //TODO: hot to fall back if pos==None
+            // TODO: hot to fall back if pos==None
+            // that'll happen if the rope is empty, and maybe other reasons
             if let Some(pos) = pos {
+                // pos is either top left, or bottom left, platfrom dependent
+                // Therefor we only use pos for x
+                let mut x = pos.point.x;
 
-                // This is either top left, or bottom left, platfrom dependent
-                let center = pos.point;
-                // I'm still not sure of druid's coord system, but this seems to work
-                // On macOS, center.y - font_size works
-                // This is right on gnu/linux/gtk
-                // I haven't checked on windows
-                let top = Point::new(center.x, center.y + font_size);
-                let bottom = Point::new(center.x, center.y);
-                
-                //dbg!(center);
+                // Font size: 15
+                // Text height: 12
+                // Line | Bottom y | Top y
+                // -----|----------|------
+                // 0    | 0        | 15
+                // 1    | 19       | 34
+                // 2    | 38       | 53
+                // on macOS with JetBrains mono
 
+                // Calculate the top and bottom y coords
+                // TODO: figure out where this number is from
+                // I got it by debuging  pos.point.y (see table above)
+                // This doesn't work if you cange the font to arial
+                let magic_number = 4.0;
+                let line_spacing = font_size + magic_number;
+                let topy = lineno * line_spacing;
+                let bottomy = ((lineno + 1.0) * line_spacing) - magic_number;
+
+                // If we are just past a newline, the position thinks we're on the line
+                // above, so misreports. Here we abjust it. y is fine as it is calculated
+                // seperayly from rope data, which gets this right.
+                if data.rope().chars_at(data.curser().saturating_sub(1)).next() == Some('\n') {
+                    x = 1.0;
+                }
+                let top = Point::new(x, topy);
+                let bottom = Point::new(x, bottomy);
                 let line = Line::new(top, bottom);
+
                 rc.stroke(line, &cursor_color, 1.0)
             }
 
