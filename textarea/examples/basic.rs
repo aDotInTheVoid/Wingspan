38188086b62a7f3a1a9c91f24ae42b3c8acb5eb4
Nixon Enraght-Moony <nixon.emoony@gmail.com>
2020-08-15 16:58:56 +0100

    Ask which file
    for testing

--- textarea/examples/basic.rs
+++ textarea/examples/basic.rs
@@ -1,5 +1,7 @@
 use color_eyre::eyre::Result;
 use druid::{AppLauncher, LocalizedString, WindowDesc};
+use std::io;
+use std::io::prelude::*;
 use std::{error::Error, fs::File, io::BufReader};
 use textarea::TextArea;
 use textedit::EditableText;
@@ -14,8 +16,14 @@ fn main() -> Result<(), Box<dyn Error>> {
         .title(WINDOW_TITLE)
         .window_size((400.0, 400.0));
 
-    let data =
-        EditableText::from_reader(BufReader::new(File::open("Cargo.toml")?))?;
+    print!("Which file to open: ");
+    io::stdout().flush()?;
+    let mut file_string = String::new();
+    io::stdin().read_line(&mut file_string)?;
+
+    let data = EditableText::from_reader(BufReader::new(File::open(
+        file_string.trim(),
+    )?))?;
 
     AppLauncher::with_window(main)
         .launch(data)
