0e854ca224a53b21eb84a2f7c2a4ca2c036c91f3
Nixon Enraght-Moony <nixon.emoony@gmail.com>
2020-08-15 16:32:47 +0100

    Use color eyre

--- textarea/examples/basic.rs
+++ textarea/examples/basic.rs
@@ -1,3 +1,4 @@
+use color_eyre::eyre::Result;
 use druid::{AppLauncher, LocalizedString, WindowDesc};
 use std::{error::Error, fs::File, io::BufReader};
 use textarea::TextArea;
@@ -7,11 +8,14 @@ const WINDOW_TITLE: LocalizedString<EditableText> =
     LocalizedString::new("Hello World!");
 
 fn main() -> Result<(), Box<dyn Error>> {
+    color_eyre::install()?;
+
     let main = WindowDesc::new(TextArea::new)
         .title(WINDOW_TITLE)
         .window_size((400.0, 400.0));
 
-    let data = EditableText::new_text("h");
+    let data =
+        EditableText::from_reader(BufReader::new(File::open("Cargo.toml")?))?;
 
     AppLauncher::with_window(main)
         .launch(data)
